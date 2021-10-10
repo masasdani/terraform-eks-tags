50e0fd23e1f9112daf9c0eef5b529dc42dbc56e1
Dwi Fahni Denni (@zeroc0d3) <dfdenni0912@gmail.com>
2021-10-10 22:13:38 +0700

    Added docker-compose configuration

---
+++ compose/docker/ubuntu/docker-push.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# -----------------------------------------------------------------------------
+#  Docker Push Container
+# -----------------------------------------------------------------------------
+#  Author     : Dwi Fahni Denni
+#  License    : Apache v2
+# -----------------------------------------------------------------------------
+set -e
+
+export CI_REGISTRY="hub.docker.com"
+export CI_PROJECT_PATH="zeroc0d3"
+
+export IMAGE="$CI_REGISTRY/$CI_PROJECT_PATH/ubuntu"
+export TAG="20.04"
+
+echo " Push Image => $IMAGE:$TAG"
+docker push $IMAGE:$TAG
